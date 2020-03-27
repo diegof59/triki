b421506c6e930242fae8e75cbbd6666be5e5afa3
diegof95 <diego.botero@correounivalle.edu.co>
2020-03-27 18:03:30 -0500

    Square ahora es un componente función.

--- src/index.js
+++ src/index.js
@@ -3,17 +3,15 @@ import ReactDOM from 'react-dom'
 import './index.css'
 
 
-class Square extends React.Component {
+function Square(props) {
 
-  render() {
     return (
       <button
         className="square"
-        onClick={() => this.props.onClick()}>
-          {this.props.value}
+        onClick={() => props.onClick()}>
+          {props.value}
       </button>
     );
-  }
 }
 
 class Board extends React.Component {
